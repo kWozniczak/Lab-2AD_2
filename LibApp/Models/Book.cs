119ef1832190bbc9c009f8f1a7bb247c16b2be58
kWozniczak <wozniczak.karolina1997@gmail.com>
2023-11-25 22:51:51 +0100

    Added Genre model and updated Book model

--- LibApp/Models/Book.cs
+++ LibApp/Models/Book.cs
@@ -1,9 +1,16 @@
-﻿namespace LibApp.Models
+﻿using Humanizer.Localisation;
+
+namespace LibApp.Models
 {
     public class Book
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
+        public Genre Genre { get; set; }
+        public byte GenreId { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public int NumberInStock { get; set; }
     }
 }
